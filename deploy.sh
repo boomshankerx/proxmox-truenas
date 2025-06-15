b46243ce2b760e05ad4b2df172d33b993a6e1e04
Lorne Guse <boomshankerx@hotmail.com>
2025-06-15 14:55:30 -0600

    Updated ZFSPlugin.pm.patch for latest code tidy.

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,23 @@ rsync -av --relative perl5/ /usr/share/
 #ZFSPlugin
 cd perl5/PVE/Storage
 diff -ruN ZFSPlugin.pm.orig ZFSPlugin.pm > ZFSPlugin.pm.patch
+cp -v ZFSPlugin.pm.orig /usr/share/perl5/PVE/Storage/ZFSPlugin.pm
 cd -
-cp -v /usr/share/perl5/PVE/Storage/ZFSPlugin.pm.orig /usr/share/perl5/PVE/Storage/ZFSPlugin.pm
 patch -p0 --verbose -d /usr/share/perl5/PVE/Storage < perl5/PVE/Storage/ZFSPlugin.pm.patch
 
 
 # pve-manager
 cd pve-manager/js/
 diff -ruN pvemanagerlib.js.orig pvemanagerlib.js >pvemanagerlib.js.patch
-cd -
 cp -v /usr/share/pve-manager/js/pvemanagerlib.js.orig /usr/share/pve-manager/js/pvemanagerlib.js
+cd -
 patch -p0 --verbose -d /usr/share/pve-manager/js/ <pve-manager/js/pvemanagerlib.js.patch
-
 
 # pve-docs/api-viewer
 cd pve-docs/api-viewer
 diff -ruN apidoc.js.orig apidoc.js >apidoc.js.patch
-cd -
 cp -v /usr/share/pve-docs/api-viewer/apidoc.js.orig /usr/share/pve-docs/api-viewer/apidoc.js
+cd -
 patch -p0 --verbose -d /usr/share/pve-docs/api-viewer <pve-docs/api-viewer/apidoc.js.patch
 
 service pve-cluster restart && service pvedaemon restart && service pvestatd restart && service pveproxy restart
