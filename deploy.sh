ee0ae68e9bf7181577f11e2bf6d7584c62c16b87
Lorne Guse <boomshankerx@hotmail.com>
2025-08-09 19:52:21 -0600

    fix: Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ dpkg-query -W libpve-storage-perl
 PATH_APIDocs="/usr/share/pve-docs/api-viewer/apidocs.js"
 PATH_ZFSPlugin="/usr/share/perl5/PVE/Storage/ZFSPlugin.pm"
 PATH_Manager="/usr/share/pve-manager/js/pvemanagerlib.js"
-PATCH_ARGS="-p0 -b --ignore-whitespace --verbose"
+PATCH_ARGS="-p1 -b --ignore-whitespace --verbose"
 
 ver=$(dpkg-query -W proxmox-ve | awk '{ print $2}' | cut -d'.' -f1)
 
@@ -40,11 +40,11 @@ fi
 
 # Patching ZFSPlugin.pm
 echo "[+] Patching ZFSPlugin.pm..."
-patch ${PATCH_ARGS} -d /usr/share/perl5/PVE/Storage < perl5/PVE/Storage/ZFSPlugin.pm.${ver}.patch
+patch ${PATCH_ARGS} /usr/share/perl5/PVE/Storage/ZFSPlugin.pm < perl5/PVE/Storage/ZFSPlugin.pm.${ver}.patch
 
 # Patching pvemanagerlib.js
 echo "[+] Patching pvemanagerlib.js..."
-patch ${PATCH_ARGS} -d /usr/share/pve-manager/js < pve-manager/js/pvemanagerlib.js.${ver}.patch
+patch ${PATCH_ARGS} /usr/share/pve-manager/js/pvemanagerlib.js < pve-manager/js/pvemanagerlib.js.${ver}.patch
 
 # echo "[+] Patching API docs..."
 # patch ${PATCH_ARGS} -d /usr/share/pve-docs/api-viewer < pve-docs/api-viewer/apidoc.js.${ver}.patch
@@ -52,5 +52,12 @@ patch ${PATCH_ARGS} -d /usr/share/pve-manager/js < pve-manager/js/pvemanagerlib.
 echo "[+] Copying TrueNAS Client..."
 rsync perl5/TrueNAS /usr/share/perl5/ -av --delete
 
+echo "[+] Copying TrueNAS Storage Plugins..."
+cp perl5/PVE/Storage/LunCmd/TrueNAS.pm /usr/share/perl5/PVE/Storage/LunCmd/
+cp perl5/PVE/Storage/TrueNAS.pm /usr/share/perl5/PVE/Storage/ 
+
 echo "[+] Restarting Proxmox services..."
-systemctl restart pve-cluster && systemctl restart pvedaemon && systemctl restart pvestatd && systemctl restart pveproxy
+systemctl restart pve-cluster
+systemctl restart pvedaemon
+systemctl restart pvestatd
+systemctl restart pveproxy
