2e3203f59cb3a928dd41c828eb634d3f01794d5d
Lorne Guse <boomshankerx@hotmail.com>
2025-08-14 16:12:35 -0600

    fix: updated pull.sh to reinstall libraries from source

--- pull.sh
+++ pull.sh
@@ -2,5 +2,7 @@
 
 ver=$(dpkg-query -W pve-manager | awk '{print $2}' | cut -d. -f1)
 
-cp -r /usr/share/perl5/PVE/Storage/ZFSPlugin.pm perl5/PVE/Storage/ZFSPlugin.pm.$ver.orig
-cp -r /usr/share/pve-manager/js/pvemanagerlib.js pve-manager/js/pvemanagerlib.js.$ver.orig
+apt reinstall pve-manager libpve-storage-perl -y
+
+cp -vr /usr/share/perl5/PVE/Storage/ZFSPlugin.pm perl5/PVE/Storage/ZFSPlugin.pm.$ver.orig
+cp -vr /usr/share/pve-manager/js/pvemanagerlib.js pve-manager/js/pvemanagerlib.js.$ver.orig
