d1d2fef66167b7d5c9d40843a1ab9b7cb15944af
Lorne Guse <boomshankerx@hotmail.com>
2025-08-14 18:12:06 -0600

    fix: cleanup dev scripts

--- pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-ver=$(dpkg-query -W pve-manager | awk '{print $2}' | cut -d. -f1)
-
-cp -r /usr/share/perl5/PVE/Storage/ZFSPlugin.pm perl5/PVE/Storage/ZFSPlugin.pm.$ver.orig
-cp -r /usr/share/pve-manager/js/pvemanagerlib.js pve-manager/js/pvemanagerlib.js.$ver.orig
